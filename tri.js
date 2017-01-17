f4f64819272e7f5ca7a553b0f7ae3a1f99872240
sadrisya <sadrisyalohid@gmail.com>
2017-01-17 10:40:13 +0530

    Add files via upload

---
+++ tri.js
@@ -0,0 +1,37 @@
+$("body").click(function(e) {
+
+
+
+  // Remove any old one
+  $(".ripple").remove();
+
+  // Setup
+  var posX = $(this).offset().left,
+    posY = $(this).offset().top,
+    bodyWidth = $(this).width(),
+    bodyHeight = $(this).height();
+
+  // Add the element
+  $(this).prepend("<span class='ripple'></span>");
+
+  // Make it round!
+  if (bodyWidth >= bodyHeight) {
+    bodyHeight = bodyWidth;
+  } else {
+    bodyWidth = bodyHeight;
+  }
+
+  // Get the center of the element
+  var x = e.pageX - posX - bodyWidth / 2;
+  var y = e.pageY - posY - bodyHeight / 2;
+
+  // Add the ripples CSS and start the animation
+  $(".ripple").css({
+    width: bodyWidth,
+    height: bodyHeight,
+    left: x + 'px',
+   
+    top: y + 'px',
+    
+  }).addClass("rippleEffect");
+});
